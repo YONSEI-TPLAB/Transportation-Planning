5f31fdf721ebc3c56b0030f760526e53c1dbf576
Sung <49590998+4Urban@users.noreply.github.com>
2022-11-21 02:20:50 +0900

    FEAT: Add Choice

---
+++ Choice.R
@@ -0,0 +1,48 @@
+
+# Activate Library --------------------------------------------------------
+
+library(readxl)
+library(tidyverse)
+
+
+# Data Input --------------------------------------------------------------
+
+df_long <- read_excel('data/data_choice.xlsx', sheet='long')
+df_wide <- read_excel('data/data_choice.xlsx', sheet='wide')
+
+
+# Activate 'mlogit' library -----------------------------------------------
+
+# install.packages('mlogit')
+library(mlogit)
+
+
+# mlogit ------------------------------------------------------------------
+
+long <- mlogit.data(df_long, choice='choice', idx=c('id', 'mode'))
+wide <- mlogit.data(df_wide, choice='choice', varying=3:10, sep='_')
+
+model_long <- mlogit(choice~vcost+wait|income+size|travel, data=long)
+model_wide <- mlogit(choice~price+time|comfort, data=wide)
+
+summary(model_long)
+summary(model_wide)
+
+
+# Prediction --------------------------------------------------------------
+
+fitted <- data.frame(model_long$probabilities)
+fitted$pred_choice <- colnames(fitted)[apply(fitted, 1, which.max)]
+
+
+# Comparison --------------------------------------------------------------
+
+df_long %>%
+  filter(choice=='yes') %>%
+  select(mode) %>%
+  table() %>%
+  prop.table()
+
+model_long$freq %>% prop.table()
+
+fitted %>% select(pred_choice) %>% table() %>% prop.table()
